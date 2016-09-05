4a90e46fd74ed3c30957d855342978f141cd9831
Arvin Quilao <arquilao@gmail.com>
2016-09-05 16:20:10 +0000

    Revert "Use stock kernel"
    This reverts commit 9b97d289d754814cb1dbaef69fd7fed15e9eb4a7.

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,18 +1,16 @@
-#BOARD_DTBTOOL_ARGS := -2
+BOARD_DTBTOOL_ARGS := -2
 
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_KERNEL_SEPARATED_DT := true
-TARGET_KERNEL_APPEND_DTB := true
+BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 ENABLE_CPUSETS := true
 
-TARGET_KERNEL_SOURCE := kernel/google/seed
+TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 ifneq ($(FORCE_32_BIT),true)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
