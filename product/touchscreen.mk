f6da8017193a50b9141d293d109da4a34514738f
dev-harsh1998 <harshit.india@linaro.org>
2017-07-14 22:06:05 +0530

    a6000-64: display: Stop shipping OTM1287_720p panel calibration data
    * No need of it for now
    * Also shift synaptics_dsx_fw_update_boyi.bin to configs directory
    
    Change-Id: I389c2d510fd1be216d2483b9c8239b69b1d4b81c

--- product/touchscreen.mk
+++ product/touchscreen.mk
@@ -4,4 +4,4 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/display/synaptics_dsx_fw_update_boyi.bin:system/etc/firmware/synaptics_dsx_fw_update_boyi.bin
+    $(LOCAL_PATH)/configs/synaptics_dsx_fw_update_boyi.bin:system/etc/firmware/synaptics_dsx_fw_update_boyi.bin
