47c11eaa1c90dc66f9849fc533d8c06422b93653
dev-harsh1998 <harshit.india@linaro.org>
2017-07-23 22:46:40 +0530

    board: kernel: add missed TARGET_USES_UNCOMPRESSED_KERNEL flag
    * Fixes build
    
    Change-Id: Id6f54c0491b272badc22df0d114b1f1d5d8078f7

--- board/kernel.mk
+++ board/kernel.mk
@@ -6,6 +6,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
+TARGET_USES_UNCOMPRESSED_KERNEL := true
 ENABLE_CPUSETS := true
 TARGET_KERNEL_SOURCE := kernel/lenovo/a6000
 TARGET_KERNEL_CONFIG := omnirom_a6000-64_defconfig
